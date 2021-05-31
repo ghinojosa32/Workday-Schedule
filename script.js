c9cd0e63981cb190405693824d35c86ec6a31967
Gustavo <gusbuss_1@outlook.com>
2021-05-31 17:41:05 -0500

    I added the link for java to html file, I also created the variables for the functions in javascript

---
+++ script.js
@@ -0,0 +1,22 @@
+// adding moment variables 
+var currentDate = moment().format('dddd') + " " + moment().format("Do MMM YYYY");
+//adding the variable for each hour
+var eightAm = $("#8am");
+var eightAm = $("#9am");
+var eightAm = $("#10am");
+var eightAm = $("#11am");
+var eightAm = $("#12pm");
+var eightAm = $("#1pm");
+var eightAm = $("#2pm");
+var eightAm = $("#3pm");
+var eightAm = $("#4pm");
+var eightAm = $("#5pm");
+var eightAm = $("#6pm");
+var eightAm = $("#7pm");
+var eightAm = $("#8pm");
+var eightAm = $("#9pm");
+
+var hour = moment().hours();
+var userInput;
+var hourSpan;
+
